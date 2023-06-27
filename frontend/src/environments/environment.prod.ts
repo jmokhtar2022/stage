acbc870f9470659ddc41b35d972d666c3670595d
jmokhtar2022 <mohamedmokhtar.jaafar@esprit.tn>
2023-06-27 20:53:55 +0100

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: true,
   appInsights: {
-    instrumentationKey: '04dc1ea3-cee7-48ec-8042-8c736ac75cd5'
+    instrumentationKey: '04dc1ea3-cee7-48ec-8042-8c736ac75cd5',
+    config: {
+      autoTrackPageVisitTime: true,
+    }
 }
 };
 export const apiUrl = '//4.157.178.132:8080/api';
