a0899dbd00af5b6f2c2fb5b857dac4f029b5a85b
jmokhtar2022 <mohamedmokhtar.jaafar@esprit.tn>
2023-06-27 18:17:05 +0100

    update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,5 +9,24 @@ import { MyMonitoringService } from './services/logging.service';
 export class AppComponent {
   constructor(private myMonitoringService: MyMonitoringService) {}
   title = 'shop';
+  ngOnInit(){
+    const myPromise = new Promise((resolve, reject) => {
+      fetch('https://jsonplaceholder.typicode.com/posts/-1')
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data)
+      })
+      .catch((error) => reject(error))
+    });
+    
+    console.log('this is the start');
+    
+    myPromise
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('promise done'));
+    
+    console.log('this is the end');
+  }
 
 }
